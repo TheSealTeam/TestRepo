3d97e9c1e7a6b206547d9242293de04b0135516c
Simon Olofsson <zigge112@gmail.com>
2016-10-19 15:11:33 +0200

    Add a for loop aswell as a printout

--- TestRepositoryProject/TestRepositoryProject/main.cpp
+++ TestRepositoryProject/TestRepositoryProject/main.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main() {
-	//First code 
+	//First code for initial commit
 	int a = 5;
 	int b = 10;
 	int c = a + b;
 	cout << "c has the value: " << c << endl;
+
+	//2nd Code 
+	for (int i = 0; i < 3; i++) {
+		cout << "i has the value : " << i << endl;
+	}
 	system("pause");
 	return 0;
 }
