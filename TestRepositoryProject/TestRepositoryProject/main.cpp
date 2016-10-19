89492612c72b498fe471227c015854a452742e5a
Simon Olofsson <zigge112@gmail.com>
2016-10-19 15:01:46 +0200

    Added the TestRepositoryProject to the Repository

---
+++ TestRepositoryProject/TestRepositoryProject/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//First code 
+	int a = 5;
+	int b = 10;
+	int c = a + b;
+	cout << "c has the value: " << c << endl;
+	system("pause");
+	return 0;
+}
